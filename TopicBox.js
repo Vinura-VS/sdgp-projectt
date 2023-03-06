b391e18ac233f0d9e11f4df81ef7f4ffb532fe5e
Vinura-VS <118790805+Vinura-VS@users.noreply.github.com>
2023-03-06 20:59:09 +0530

    Update TopicBox.js

--- TopicBox.js
+++ TopicBox.js
@@ -4,8 +4,19 @@ export default function TopicBox() {
   return (
     <div>
      <div className="topicBox">
-        <span className='text'>This is the topic</span>
+        <span className='text'>Efficency Tracker</span>
+        
+        <h2>If you are?</h2>
+<form>
+  <input type="submit" value="EMPLOYEE" />
+</form>  
+
+<form>
+  <input type="submit" value="EMPLOYER" />
+</form>  
+
      </div>
     </div>
   )
 }
+
